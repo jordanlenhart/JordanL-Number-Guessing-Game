a7f88cb68a579f05bef5d009d8021fcff4f2078a
jordanlenhart <jordinholenhart@gmail.com>
2025-04-09 16:10:34 +0000

    created script.js and the while loop that uses console.log as the game. The laptop I am using is too slow and overwhelmed by the simple js, will add in CSS and test on home desktop.

---
+++ script.js
@@ -0,0 +1,14 @@
+// const randomNumber = Math.floor(Math.random() * 100) + 1;
+const randomNumber = 12;
+let userGuess = null;
+
+while (userGuess != randomNumber) {
+    userGuess = parseInt(prompt("Guess a number between 1 and 100:"));
+    if (userGuess < randomNumber) {
+        console.log("Too low! Try Again.");
+    } else if (userGuess > randomNumber) {
+        console.log("Too high! Try Again.");
+    } else if (userGuess == randomNumber) {
+        console.log("Congratulations! You guessed the correct number!")
+    }
+}
